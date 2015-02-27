8ff9a1cde9e9eb1e33ada736031795606f2b4c46
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-27 01:56:38 +0000

    fix nose-traceback-info inclusion per greg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.client_preferences/trunk@60410 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,43 +4,37 @@ import codecs
 VERSION = '0.0.0'
 
 entry_points = {
-    "z3c.autoinclude.plugin": [
+	"z3c.autoinclude.plugin": [
 		'target = nti.app',
 	],
 }
 
 setup(
-    name = 'nti.app.client_preferences',
-    version = VERSION,
-    author = 'Jason Madden',
-    author_email = 'jason@nextthought.com',
-    description = "Support for client readable/writable preferences",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'pyramid preference',
-    #url = 'https://github.com/NextThought/nti.nose_traceback_info',
-    classifiers = [
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name = 'nti.app.client_preferences',
+	version = VERSION,
+	author = 'Jason Madden',
+	author_email = 'jason@nextthought.com',
+	description = "Support for client readable/writable preferences",
+	long_description = codecs.open('README.rst', encoding='utf-8').read(),
+	license = 'Proprietary',
+	keywords = 'pyramid preference',
+	classifiers = [
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.3',
-        'Topic :: Software Development :: Testing'
+		'Topic :: Software Development :: Testing'
 		'Framework :: Pyramid',
-        ],
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti', 'nti.app'],
 	install_requires=[
 		'setuptools',
 		'zope.preference'
-		# NOTE: We actually depend on nti.dataserver
-		# as well, but for the sake of legacy
-		# deployments, we do not yet declare that.
-		# We will declare it when everything is in
-		# buildout
 	],
 	entry_points=entry_points
 )
