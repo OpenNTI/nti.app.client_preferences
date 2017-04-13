841827a3aa810c0f481f4b83e53f7971c6a91fff
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-13 20:26:35 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.client_preferences/trunk@110699 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/client_preferences/pyramid.py
+++ src/nti/app/client_preferences/pyramid.py
@@ -21,33 +21,35 @@ from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtils
 
 from nti.dataserver import authorization as nauth
 
-@view_config(route_name='objects.generic.traversal',
-			 request_method='GET',
-			 renderer='rest',
-			 context=IPreferenceGroup,
-			 permission=nauth.ACT_READ)
-def PreferencesGetView(request):
-	# This checks adaptation to annotations
-	# and the security interaction all at the same time
-	# Because we load the ++preference++ traversal namespace,
-	# this is available at /path/to/principal/++preference++
-	# (and sub-paths, nice! for automatic fetch-in-part)
-	# TODO: We should supply etag and/or last modified for this
-	# (does the default etag kick in?)
-	return request.context
 
 @view_config(route_name='objects.generic.traversal',
-			 request_method='PUT',
-			 renderer='rest',
-			 context=IPreferenceGroup,
-			 permission=nauth.ACT_UPDATE)
-class PreferencesPutView(AbstractAuthenticatedView, 
-						 ModeledContentUploadRequestUtilsMixin):
-	# Although this is the UPDATE permission,
-	# the prefs being updated are always those of the current user
-	# implicitly, regardless of traversal path. We could add
-	# an ACLProvider (and hook into the zope checker machinery?)
-	# but that would be primarily for aesthetics
-	def __call__(self):
-		externalValue = self.readInput( )
-		return self.updateContentObject(self.request.context, externalValue, notify=False)
+             request_method='GET',
+             renderer='rest',
+             context=IPreferenceGroup,
+             permission=nauth.ACT_READ)
+def PreferencesGetView(request):
+    # This checks adaptation to annotations
+    # and the security interaction all at the same time
+    # Because we load the ++preference++ traversal namespace,
+    # this is available at /path/to/principal/++preference++
+    # (and sub-paths, nice! for automatic fetch-in-part)
+    # TODO: We should supply etag and/or last modified for this
+    # (does the default etag kick in?)
+    return request.context
+
+
+@view_config(route_name='objects.generic.traversal',
+             request_method='PUT',
+             renderer='rest',
+             context=IPreferenceGroup,
+             permission=nauth.ACT_UPDATE)
+class PreferencesPutView(AbstractAuthenticatedView,
+                         ModeledContentUploadRequestUtilsMixin):
+    # Although this is the UPDATE permission,
+    # the prefs being updated are always those of the current user
+    # implicitly, regardless of traversal path. We could add
+    # an ACLProvider (and hook into the zope checker machinery?)
+    # but that would be primarily for aesthetics
+    def __call__(self):
+        externalValue = self.readInput()
+        return self.updateContentObject(self.request.context, externalValue, notify=False)
