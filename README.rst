bda37e4092bf4de348c6e178eb3629ca9042d846
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-27 00:47:19 +0000

    add docs; update supporting files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.client_preferences/trunk@120565 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
+============================
+ nti.app.client_preferences
+============================
 
+.. image:: https://travis-ci.org/NextThought/nti.app.client_preferences.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.client_preferences
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.client_preferences/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.client_preferences?branch=master
--- setup.py
+++ setup.py
@@ -1,40 +1,52 @@
-from setuptools import setup, find_packages
 import codecs
-
-VERSION = '0.0.0'
+from setuptools import setup, find_packages
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
 }
 
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name = 'nti.app.client_preferences',
-	version = VERSION,
-	author = 'Jason Madden',
-	author_email = 'jason@nextthought.com',
-	description = "Support for client readable/writable preferences",
-	long_description = codecs.open('README.rst', encoding='utf-8').read(),
-	license = 'Proprietary',
-	keywords = 'pyramid preference',
-	classifiers = [
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-		'Topic :: Software Development :: Testing'
-		'Framework :: Pyramid',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app'],
-	install_requires=[
-		'setuptools',
-		'zope.preference'
-	],
-	entry_points=entry_points
+    name='nti.app.client_preferences',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="Support for client readable/writable preferences",
+    long_description=_read('README.rst'),
+    license='Apache',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    url="https://github.com/NextThought/nti.app.client_preferences",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app'],
+    install_requires=[
+        'setuptools',
+        'zope.preference',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
 )
